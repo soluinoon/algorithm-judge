87bd006d400955c1c5404f4f7e720476cbc522ef
eyeofsol <kingjihong@naver.com>
2022-12-27 15:50:23 +0900

    12-24 1712

---
+++ basic_math/1712.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    /**
+     * 1712
+     * 손익분기점
+     * 22-12-27
+     */
+    static long base, makingPrice, sellPrice;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        base = Integer.parseInt(st.nextToken());
+        makingPrice = Integer.parseInt(st.nextToken());
+        sellPrice = Integer.parseInt(st.nextToken());
+
+        if (makingPrice > sellPrice) {
+            System.out.println(-1);
+            return;
+        }
+
+        System.out.println((base / (sellPrice - makingPrice)) + 1);
+    }
+}
