6e53689185e35bc35946f8f830290ed40e9f7d6a
Jihong Kim <kingjihong@naver.com>
2023-06-19 20:48:00 +0900

    add: 백준 절댓값 힙

---
+++ heap/boj11286.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((o1, o2) -> {
+            if (Math.abs(o1) == Math.abs(o2)) {
+                return o1 - o2;
+            } else {
+                return Math.abs(o1) - Math.abs(o2);
+            }
+        });
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            int number = Integer.parseInt(br.readLine());
+            if (number == 0) {
+                if (priorityQueue.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(priorityQueue.poll());
+                }
+            } else {
+                priorityQueue.add(number);
+            }
+        }
+    }
+}
